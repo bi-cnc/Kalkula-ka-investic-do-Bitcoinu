6749c5c8b86802849550c1415a10cc1b131d570d
bi-cnc <137881662+bi-cnc@users.noreply.github.com>
2024-01-18 15:59:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def plot_bitcoin_data(bitcoin_data, start_date, end_date):
 # Streamlit aplikace
 def main():
     st.title("Kdybych investoval, kolik bych vydělal nebo prodělal?")
-    large_font = "<h2 style='font-size:30px; color: black;'>Kalkulačka investic do Bitcoinu</h2>"
+    large_font = "<h2 style='font-size:16px; color: black;'>Kalkulačka investic do Bitcoinu</h2>"
     st.markdown(large_font, unsafe_allow_html=True)
 
     start_date = st.date_input("Zadejte počáteční datum", datetime(2020, 1, 1))
