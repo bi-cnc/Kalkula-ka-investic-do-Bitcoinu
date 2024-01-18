ab5c04c54d3c1a027b301f033298729dad1a2c62
bi-cnc <137881662+bi-cnc@users.noreply.github.com>
2024-01-18 15:55:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def plot_bitcoin_data(bitcoin_data, start_date, end_date):
 # Streamlit aplikace
 def main():
     st.title("Kdybych investoval, kolik bych vydělal nebo prodělal?")
-    st.text("Kalkulačka investic do Bitcoinu")
+    st.markdown("*Kalkulačka investic do Bitcoinu*")
 
     start_date = st.date_input("Zadejte počáteční datum", datetime(2020, 1, 1))
     end_date = st.date_input("Zadejte koncové datum", datetime.now())
